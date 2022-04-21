f70b00ede39e376df3a33b941aa42c9a72567a31
MIQ87 <miq3l@wp.pl>
2022-04-21 11:58:31 +0200

    fixed

--- src/main/java/pl/miq3l/bttconnect/api/MainApi.java
+++ src/main/java/pl/miq3l/bttconnect/api/MainApi.java
@@ -1,14 +1,15 @@
 package pl.miq3l.bttconnect.api;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping
 public class MainApi {
     @GetMapping
-    public String index() {
-        return "It's ok";
+    public ResponseEntity<String> ok() {
+        return ResponseEntity.ok("OK");
     }
 }
